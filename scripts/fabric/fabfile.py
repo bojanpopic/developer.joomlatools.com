24b358070b92bcc29b6b4bb3f80a12bb9f7db4cf
Steven Rombauts <steven@timble.net>
2014-08-25 16:50:34 +0200

    Add fabric deploy script

---
+++ scripts/fabric/fabfile.py
@@ -0,0 +1,21 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+from fabric.colors import yellow
+
+def production():
+    env.hosts = ['deploy@184.106.83.177:9791']
+
+# Deploy method:
+def deploy():
+    git_directory = '/var/www/developer.joomlatools.com/git'
+    with cd(git_directory):
+        print(yellow("-- Git: Remove staged and working directory changes"))
+        run("git reset --hard")
+        print(yellow("-- Git: Remove untracked files"))
+        run("git clean -f -d")
+        print(yellow("-- Git: pull from origin"))
+        run("git pull origin")
+        print(yellow("-- Build GitBook"))
+        run("gitbook build .")
+        run("[ -f ./index.html ] && rsync -az ./ ../public/ || echo 'Build failed!'")
